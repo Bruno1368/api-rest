d9c6163ba3c95d779e0cabcb8a6d73ac5ead46c2
Bruno <bruno.luis.999@hotmail.com>
2023-12-28 14:26:48 -0300

    criando o método excluir utilizando o http verbo delete

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -38,5 +38,11 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+
+    }
 
 }
