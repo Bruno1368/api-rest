3f71205c724b9aaece8c6b6b57338fd10cff92d9
Bruno <bruno.luis.999@hotmail.com>
2024-01-11 13:36:15 -0300

    Fim do método security filter

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -5,6 +5,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,23 +17,35 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         var tokenJWT = recuperarToken(request);
+        if(tokenJWT != null){// ta vindo o token
+            var subject = tokenService.getSubject(tokenJWT);// se checou, valida, se está válida, pegar o subject, o login(email) entao:
+            var usuario = repository.findByLogin(subject); // encontra o objeto usuario pelo login, subject, com o repositorio
 
-        System.out.println(tokenJWT);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities()); // dto do spring
+            SecurityContextHolder.getContext().setAuthentication(authentication); // força  autenticação
+        }
+
+
 
         filterChain.doFilter(request, response);// passar para o próximo filtro
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader =  request.getHeader("Authorization");// nome do cabeçalho, a aplicação frontend deve mandar o token gerado pelo backend via cabeçalho
-        if(authorizationHeader != null){ // se não existir cabeçalho é devolvido nulo
-            return authorizationHeader.replace("Bearer", "");
-        }else {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho authorization");
+
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer", "").trim();
         }
+        return null;
     }
 }
