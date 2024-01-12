4574c8a00caca96f854897a8f6c064d93ebc39d0
Bruno <bruno.luis.999@hotmail.com>
2024-01-12 16:07:03 -0300

    criando a classe validador de horário de funcionamento da clínica: O horário de funcionamento da clínica é de segunda a sábado, das 07:00 às 19:00;

---
+++ src/main/java/med/voll/api/domain/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        var depoisFechamentoDaClinica = dataConsulta.getHour() > 18;
+
+        if(domingo || antesDaAberturaDaClinica || depoisFechamentoDaClinica){}
+            throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica");
+    }
+
+}
